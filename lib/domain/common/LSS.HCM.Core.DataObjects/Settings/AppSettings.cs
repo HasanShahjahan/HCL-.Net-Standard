0a5aa27de8f79392cfe4cadcba64f10cb8437594
Hasan <blackbee08@gmail.com>
2021-01-04 14:35:40 +0700

    Configurable photo snapshot

--- lib/domain/common/LSS.HCM.Core.DataObjects/Settings/AppSettings.cs
+++ lib/domain/common/LSS.HCM.Core.DataObjects/Settings/AppSettings.cs
@@ -2,7 +2,6 @@
 
 namespace LSS.HCM.Core.DataObjects.Settings
 {
-   // [Serializable]
     public class AppSettings
     {
         public Microcontroller Microcontroller { get; set; }
@@ -10,5 +9,6 @@ namespace LSS.HCM.Core.DataObjects.Settings
         public Buzzer Buzzer { get; set; }
         public Socket Socket { get; set; }
         public LoggerInfo Logger { get; set; }
+        public CaptureImage CaptureImage { get; set; }
     }
 }
